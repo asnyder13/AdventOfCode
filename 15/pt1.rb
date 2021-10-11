20ed78e8058c310c2fdccb08136aea31a1ddfafe
Andrew Snyder <snyder13@vt.edu>
2021-10-11 18:10:38 -0400

    Neato default Hash construction

--- 15/pt1.rb
+++ 15/pt1.rb
@@ -6,7 +6,8 @@ lines = ARGF.readlines
 
 initial_values = lines[0].split(',').map(&:chomp).map(&:to_i)
 spoken = initial_values
-positions = initial_values.each.with_index.to_h { [_1, [_2]] }
+positions = Hash.new { |h, v| h[v] = [] }
+initial_values.each.with_index { positions[_1] << _2 }
 
 (spoken.length...2020).each do |i|
 	this_num = 0
@@ -16,7 +17,6 @@ positions = initial_values.each.with_index.to_h { [_1, [_2]] }
 	end
 
 	spoken << this_num
-	positions[this_num] = [] unless positions.key?(this_num)
 	positions[this_num] << i
 end
 
