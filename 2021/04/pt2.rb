d7839f9e46850c267a02c941653cadd348c178ad
Andrew Snyder <snyder13@vt.edu>
2021-12-05 12:05:35 -0500

    2012 04 refactor

--- 2021/04/pt2.rb
+++ 2021/04/pt2.rb
@@ -1,47 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative './bingo_board'
+
 abort('Please provide input file') if ARGV.count.zero?
 # This way sorbet gives at least _some_ hints.
 lines = []
 lines.concat ARGF.readlines(chomp: true)
-
-# Bingo Board
-class BingoBoard
-	attr_reader :rows, :marked, :count_row, :count_col, :bingo_draw
-
-	def initialize
-		@rows = []
-		@count_row = Hash.new { |h, k| h[k] = 0 }
-		@count_col = Hash.new { |h, k| h[k] = 0 }
-		@marked    = []
-		@bingo_draw = 1
-	end
-
-	def empty? = rows.empty?
-	def add_row(row) = rows << row
-	def size = rows.length
-
-	def mark(num)
-		row = rows.detect { |r| r.include? num }
-		return unless row
-
-		row_idx = rows.index row
-		col_idx = row.index num
-
-		marked << num
-		count_row[row_idx] += 1
-		count_col[col_idx] += 1
-	end
-
-	def bingo?
-		bingo = (count_row.values.include?(size) && :row) || (count_col.values.include?(size) && :col)
-		@bingo_draw += 1 unless bingo
-		bingo
-	end
-
-	def unmarked = rows.flatten.difference(marked)
-end
 
 numbers = lines.shift.split(',').map(&:to_i)
 boards = []
